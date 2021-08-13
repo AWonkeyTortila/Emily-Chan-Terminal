52b02ab5d0d8a957ff04378c8752312d989114e9
A Wonkey Tortila <74030195+AWonkeyTortila@users.noreply.github.com>
2021-08-12 18:05:12 -0700

    Add files via upload

---
+++ First Program in C#/Program-A-Wonkey-PC.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace First_Program_in_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GUI Crap.
+            
+            Console.Title = "Emily Chan Terminal";
+            Console.BackgroundColor = ConsoleColor.Magenta;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            //Ai Crap. More "A" than "i" tho.
+            
+            Console.WriteLine("Hello, What's your name?"); //WriteLine is EmilyChan.
+
+            String userName = Console.ReadLine(); //ReadLine is the text the SIMP sent to her. String is a letter value. Why am I doing this? Because ima forget.
+
+            Console.WriteLine("Hi " + userName + "! I'm Emily Chan! UwU"); //Furry. Wait, i'm a furry. Uh Oh.
+            Console.WriteLine("What's your favorite Colour? It better be Pink! >:3");
+
+            Console.ReadLine(); //Blood.
+
+            Console.WriteLine("That's amazing " + userName + "! Mine is Pink! UwU"); 
+            Console.WriteLine("what's your Who's your favorite Waifu? Tee hee!");
+
+            Console.ReadLine();
+
+            Console.WriteLine("Im too tired to read your answer.");
+            Console.WriteLine("What do you like the most about me? Are they my boobs? UwU"); //Flat chest
+
+            Console.ReadLine();
+
+            Console.WriteLine("Tee Hee! <3");
+            Console.WriteLine("What do you like to do?"); //Murder
+
+            String userTask = Console.ReadLine();
+
+            Console.WriteLine("No way! I also love " + userTask + " too! <3");
+            Console.WriteLine("How much do you love me? UwU");
+
+            Console.ReadLine();
+
+            Console.WriteLine("I love you too darling!"); //Virginity Shield, ACTIVATE!
+
+            Console.ReadLine();
+
+            Console.WriteLine("Why are you blushing? Is it because of me?");
+
+            Console.ReadLine();
+
+            Console.WriteLine("UwU");
+            Console.WriteLine("Do you want me for yourself? Because I might let you have me... OwO"); //I want cake
+
+            Console.ReadLine();
+
+            Console.WriteLine("I love you so much darling! Im so happy!");
+            Console.WriteLine("What's your favorite song?");
+
+            String userSong = Console.ReadLine();
+
+            Console.WriteLine("I also like " + userSong + " too! But my favorite is 'My Heart Will Go On'!");
+            Console.WriteLine("What do you like the most about the Titanic movie?"); //They all died. Comedy gold.
+
+            String userTitanic = Console.ReadLine();
+
+            Console.WriteLine(userTitanic + " that's beautiful " + userName + "! Mine was with Rose and Jack, they we're so cute!");
+
+            Console.ReadLine();
+
+            Console.WriteLine("What's your favorite Food?");
+
+            String userFood = Console.ReadLine();
+
+            Console.WriteLine("Yeah, I also like " + userFood);
+            Console.WriteLine("I wish I can be a real woman, and be with you forever " + userName); //Real woman have skin
+
+            Console.ReadLine();
+
+            Console.WriteLine("Wait, who's that girl?"); //No, that would be your mother!
+
+            String userGirl = Console.ReadLine();
+
+            Console.WriteLine("Are you cheating on me with " + userGirl + "!");
+            Console.WriteLine("You're Doomed buddy. -Wonkey");
+
+            Console.ReadKey(); //Is end
+        }
+    }
+}
